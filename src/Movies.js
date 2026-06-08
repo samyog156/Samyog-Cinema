e409305f4d1f60672d093576174c0098b8527741
samyog156 <belbasesam@gmail.com>
2026-06-08 14:53:36 +0545

    initial commit

--- src/Movies.js
+++ src/Movies.js
@@ -139,7 +139,7 @@ const movies = [
   },
 
   {
-    id: "0", 
+    id: "-2", 
     title: "Money Heist Season 1",
     type: "Series",
     genre: ["crime", "popular", ""],
@@ -276,6 +276,30 @@ const movies = [
   backupIframe: 
       "https://abyssplayer.com/JVVLEiNhf",
   },
+
+  {
+    id: "-1",
+    title: "12th Fail",
+    genre: ["drama","hero",],
+    type: "Movie",
+
+    overview:
+      "Based on the true story of IPS officer Manoj Kumar Sharma, 12th Fail sheds limelight on fearlessly embracing the idea of restarting the academic journey despite the setbacks and challenges and reclaiming one's destiny at a place where millions of students attempt the world's toughest competitive exam: UPSC.",
+
+    poster_path:
+      "https://image.tmdb.org/t/p/original/eebUPRI4Z5e1Z7Hev4JZAwMIFkX.jpg",
+
+    backdrop_path:
+      "https://image.tmdb.org/t/p/original/mn6QCsrr9zABlOupwyS4zTWpI28.jpg",
+
+    trailerId: "KjbtuqENvVE",
+
+    videoUrl: "",
+  
+  backupIframe: 
+      "https://abyssplayer.com/tUGoMmnW0",
+  },
+
 ];
 
 export default movies;
